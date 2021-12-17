911adbd00145d0b04607b8247febcb34a0332eb6
Evan Woods <you@example.com>
2021-12-17 11:25:42 +1100

    Added javascript functionality to the landing page. Set up the timer function to start on button click, removes the landing page section on click.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,3 +1,29 @@
-function timer() {
+landingPage = document.getElementById("landing-page");
+quizPage = document.getElementById("quiz");
+startGameButton = document.getElementById("start-game-button");
+countdown = document.getElementById("timer");
 
+// global variables for the timer
+var seconds = 60;
+countdown.textContent = "timer: " + seconds;
+
+// Function to start the timer
+function timer() {
+    setInterval(function() {
+        seconds -= 1;
+        countdown.textContent = "timer: " + seconds;
+    }, 1000);
 }
+
+
+// Function to start the game
+function startGame() {
+    startGameButton.addEventListener("click", function() {
+        timer();        // start the timer
+
+        landingPage.style.display = "none";     // remove the landing page from view
+        quizPage.style.display = "flex";      // add the quiz to the view
+    });
+}
+
+startGame()
