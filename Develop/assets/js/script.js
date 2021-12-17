b534dd0a388e1caad8804bffc96edb6439866a04
Evan Woods <you@example.com>
2021-12-17 13:04:18 +1100

    added logic to push the questions and answers to the page. Also added logic to check whether the answer is correct.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,36 @@
-landingPage = document.getElementById("landing-page");
-quizPage = document.getElementById("quiz");
-startGameButton = document.getElementById("start-game-button");
-countdown = document.getElementById("timer");
+var landingPage = document.getElementById("landing-page");
+var quizPage = document.getElementById("quiz");
+var startGameButton = document.getElementById("start-game-button");
+var countdown = document.getElementById("timer");
+
+var question = document.getElementById("question");
+var answer1 = document.getElementById("answer-1");
+var answer2 = document.getElementById("answer-2");
+var answer3 = document.getElementById("answer-3");
+var answer4 = document.getElementById("answer-4");
+
+
+// global variable for the quesations and answers
+var questionsAndAnswers = [
+    question1 = {
+        question: "Arrays in javascript can store what variables",
+        answers: {
+            incorrect1: "none",
+            incorrect2: "numbers",
+            incorrect3: "strings",
+            correct: "anything"
+        }
+    },
+    question2 = {
+        question: "the difference between a float and an int is",
+        answers: {
+            incorrect1: "nothing",
+            incorrect2: "ability to multiply",
+            incorrect3: "there are only floats in javascript",
+            correct: "decimals"        
+        }
+    },
+];
 
 // global variables for the timer
 var seconds = 60;
@@ -22,8 +51,73 @@ function startGame() {
         timer();        // start the timer
 
         landingPage.style.display = "none";     // remove the landing page from view
-        quizPage.style.display = "flex";      // add the quiz to the view
+        setAndCheckQuestions();
     });
 }
 
-startGame()
+
+// Function for the quiz - questions and answers
+function setAndCheckQuestions() {
+
+    var isCorrect = 0;
+    var isIncorrect = 0;
+
+    quizPage.style.display = "flex";      // add the quiz to the view
+
+    questionIndex = Math.floor(Math.random() * questionsAndAnswers.length);     // random question from the array
+    correctAnswerIndex = Math.floor(Math.random() * 3);     //random correct answer placement
+
+    question.textContent = questionsAndAnswers[questionIndex].question;
+
+    if (correctAnswerIndex === 0) {
+        answer1.textContent = questionsAndAnswers[questionIndex].answers.correct;
+        answer2.textContent = questionsAndAnswers[questionIndex].answers.incorrect1;
+        answer3.textContent = questionsAndAnswers[questionIndex].answers.incorrect2;
+        answer4.textContent = questionsAndAnswers[questionIndex].answers.incorrect3;
+    } else if (correctAnswerIndex === 1) {
+        answer1.textContent = questionsAndAnswers[questionIndex].answers.incorrect1;
+        answer2.textContent = questionsAndAnswers[questionIndex].answers.correct;
+        answer3.textContent = questionsAndAnswers[questionIndex].answers.incorrect2;
+        answer4.textContent = questionsAndAnswers[questionIndex].answers.incorrect3;
+    }
+    else if (correctAnswerIndex === 2) {
+        answer1.textContent = questionsAndAnswers[questionIndex].answers.incorrect1;
+        answer2.textContent = questionsAndAnswers[questionIndex].answers.incorrect2;
+        answer3.textContent = questionsAndAnswers[questionIndex].answers.correct;
+        answer4.textContent = questionsAndAnswers[questionIndex].answers.incorrect3;
+    } else if (correctAnswerIndex === 3) {
+        answer1.textContent = questionsAndAnswers[questionIndex].answers.incorrect1;
+        answer2.textContent = questionsAndAnswers[questionIndex].answers.incorrect3;
+        answer3.textContent = questionsAndAnswers[questionIndex].answers.incorrect2;
+        answer4.textContent = questionsAndAnswers[questionIndex].answers.correct;
+    }
+
+    answer1.addEventListener("click", function() {
+        if (answer1.textContent === questionsAndAnswers[questionIndex].answers.correct) {
+            isCorrect = true;
+            console.log(isCorrect);
+        } 
+    })
+    answer2.addEventListener("click", function() {
+        if (answer2.textContent === questionsAndAnswers[questionIndex].answers.correct) {
+            isCorrect = true;
+            console.log(isCorrect);
+        } 
+    })
+    answer3.addEventListener("click", function() {
+        if (answer3.textContent === questionsAndAnswers[questionIndex].answers.correct) {
+            isCorrect = true;
+            console.log(isCorrect);
+        } 
+    })
+    answer4.addEventListener("click", function() {
+        if (answer4.textContent === questionsAndAnswers[questionIndex].answers.correct) {
+            isCorrect = true;
+            console.log(isCorrect);
+        } 
+    })
+    return isCorrect, isIncorrect;
+
+}
+
+startGame();
